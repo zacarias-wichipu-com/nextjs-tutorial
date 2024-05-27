9754d8dd3e1ff4b0e9e111ee4696490cfc01c9ad
Zacarías <zacarias@wichipu.com>
2024-05-27 07:44:03 +0200

    feat: Added Suspense behaviour to latest invoices component.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -36,12 +36,17 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
+    console.log('Fetching latest invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+
+    console.log('Data fetch completed after 2 seconds.');
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
