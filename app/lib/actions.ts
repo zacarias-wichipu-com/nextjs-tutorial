08dfb91e3f4f529d0d60189ab9fd75827a4c58bb
Zacarías <zacarias@wichipu.com>
2024-06-01 07:29:25 +0200

    feat: Added invoice uodate.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,6 +12,7 @@ const FormScheme = z.object({
     status: z.enum(['pending', 'paid']),
     date: z.string()
 })
+
 const CreateInvoice = FormScheme.omit({id: true, date: true});
 
 export async function createInvoice(formData: FormData) {
@@ -33,4 +34,24 @@ export async function createInvoice(formData: FormData) {
     const invoicesPath = '/dashboard/invoices';
     revalidatePath(invoicesPath);
     redirect(invoicesPath);
+}
+
+const UpdateInvoice = FormScheme.omit({id: true, date: true});
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const {customerId, amount, status} = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId},
+            amount      = ${amountInCents},
+            status      = ${status}
+        WHERE id = ${id}
+    `;
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
