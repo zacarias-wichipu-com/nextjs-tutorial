a3ec65918caf56715ed68444981fd0537346b974
Zacarías <zacarias@wichipu.com>
2024-06-02 11:42:06 +0200

    feat: Added update invoice validation.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -19,7 +19,6 @@ const FormScheme = z.object({
     }),
     date: z.string()
 })
-const CreateInvoice = FormScheme.omit({id: true, date: true});
 
 // This is temporary until @types/react-dom is updated
 export type State = {
@@ -31,6 +30,7 @@ export type State = {
     message?: string | null;
 };
 
+const CreateInvoice = FormScheme.omit({id: true, date: true});
 export async function createInvoice(prevState: State, formData: FormData) {
     const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
@@ -38,7 +38,6 @@ export async function createInvoice(prevState: State, formData: FormData) {
         status: formData.get('status')
     });
 
-    // If form validation fails, return errors early. Otherwise, continue.
     if (!validatedFields.success) {
         return {
             errors: validatedFields.error.flatten().fieldErrors,
@@ -66,14 +65,23 @@ export async function createInvoice(prevState: State, formData: FormData) {
 }
 
 const UpdateInvoice = FormScheme.omit({id: true, date: true});
-
-export async function updateInvoice(id: string, formData: FormData) {
-    const {customerId, amount, status} = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
-        status: formData.get('status'),
+        status: formData.get('status')
     });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+
+    const {customerId, amount, status} = validatedFields.data;
     const amountInCents = amount * 100;
+
     try {
         await sql`
             UPDATE invoices
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -12,17 +12,21 @@ import {
 import Link from 'next/link';
 import {Button} from '@/app/ui/button';
 import {hidden} from "next/dist/lib/picocolors";
+import {useFormState} from "react-dom";
+import Error from "@/app/ui/invoices/error";
 
-export default function EditInvoiceForm({
-                                            invoice,
-                                            customers,
-                                        }: {
+export default function Form({
+                                 invoice,
+                                 customers,
+                             }: {
     invoice: InvoiceForm;
     customers: CustomerField[];
 }) {
+    const initialState = {message: null, errors: {}};
     const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+    const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
     return (
-        <form action={updateInvoiceWithId}>
+        <form action={dispatch}>
             <input type="hidden" name="id" value={invoice.id}/>
             <div className="rounded-md bg-gray-50 p-4 md:p-6">
                 {/* Customer Name */}
@@ -36,6 +40,7 @@ export default function EditInvoiceForm({
                             name="customerId"
                             className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                             defaultValue={invoice.customer_id}
+                            aria-describedby="customer-error"
                         >
                             <option value="" disabled>
                                 Select a customer
@@ -49,8 +54,8 @@ export default function EditInvoiceForm({
                         <UserCircleIcon
                             className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500"/>
                     </div>
+                    <Error errorId={"customer-error"} errors={state.errors?.customerId} />
                 </div>
-
                 {/* Invoice Amount */}
                 <div className="mb-4">
                     <label htmlFor="amount" className="mb-2 block text-sm font-medium">
@@ -66,13 +71,14 @@ export default function EditInvoiceForm({
                                 defaultValue={invoice.amount}
                                 placeholder="Enter USD amount"
                                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                aria-describedby="amount-error"
                             />
                             <CurrencyDollarIcon
                                 className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
                         </div>
                     </div>
+                    <Error errorId={"amount-error"} errors={state.errors?.amount} />
                 </div>
-
                 {/* Invoice Status */}
                 <fieldset>
                     <legend className="mb-2 block text-sm font-medium">
@@ -88,6 +94,7 @@ export default function EditInvoiceForm({
                                     value="pending"
                                     defaultChecked={invoice.status === 'pending'}
                                     className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                                    aria-describedby="status-error"
                                 />
                                 <label
                                     htmlFor="pending"
@@ -114,6 +121,7 @@ export default function EditInvoiceForm({
                             </div>
                         </div>
                     </div>
+                    <Error errorId={"status-error"} errors={state.errors?.status} />
                 </fieldset>
             </div>
             <div className="mt-6 flex justify-end gap-4">
