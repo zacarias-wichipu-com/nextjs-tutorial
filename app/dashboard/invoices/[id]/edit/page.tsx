0983cd5e516ad6d5cf7decc206e99acac41b159c
Zacarías <zacarias@wichipu.com>
2024-06-08 08:44:08 +0200

    feat: Added metadata.

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,8 +1,29 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import {fetchInvoiceById, fetchCustomers} from '@/app/lib/data';
-import {compareSync} from "bcrypt";
 import {notFound} from "next/navigation";
+import {Metadata, ResolvingMetadata} from "next";
+
+type Props = {
+    params: { id: string }
+    searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata(
+    {params}: Props
+): Promise<Metadata> {
+    const id = params.id
+
+    const [
+        invoice,
+    ] = await Promise.all([
+        fetchInvoiceById(id),
+    ]);
+
+    return {
+        title: `Edit Invoice ${invoice.id}`
+    }
+}
 
 export default async function Page({params}: { params: { id: string } }) {
     const id = params.id;
